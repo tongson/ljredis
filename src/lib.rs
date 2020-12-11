e9c3f6b1ff9f8c8678997d5397ef9451878052d2
Eduardo Tongson <propolice@gmail.com>
2020-12-11 14:19:19 +0800

    src: Use straight vectors. Fix incr() return.

--- src/lib.rs
+++ src/lib.rs
@@ -16,21 +16,21 @@ fn cs(s: Vec<u8>) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn incr(c: *const c_char) -> *const c_char {
-  let nak: Vec<u8> = b"\x21".to_vec();
-  let ack: Vec<u8> = b"\x06".to_vec();
-  let dc2: Vec<u8> = b"\x18".to_vec();
-  let dc4: Vec<u8> = b"\x20".to_vec();
+  let ack: Vec<u8> = vec!(6);
+  let dc2: Vec<u8> = vec!(18);
+  let dc4: Vec<u8> = vec!(20);
+  let nak: Vec<u8> = vec!(21);
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.incr()")));
   let client = match redis::Client::open(format!("redis://{}/", HOST)) {
-    Ok(client) => client,
+    Ok(rc) => rc,
     Err(_) => return cs(dc2),
   };
   let mut con = match client.get_connection() {
-    Ok(con) => con,
+    Ok(rc) => rc,
     Err(_) => return cs(dc4),
   };
   let cb = unsafe { CStr::from_ptr(c).to_bytes() };
-  let _ : () = match redis::cmd("INCR").arg(cb).query::<Option<String>>(&mut con) {
+  let _ : () = match redis::cmd("INCR").arg(cb).query::<i32>(&mut con) {
     Ok(_) => return cs(ack),
     Err(_) => return cs(nak),
   };
@@ -38,9 +38,9 @@ pub extern "C" fn incr(c: *const c_char) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn get(c: *const c_char) -> *const c_char {
-  let nak: Vec<u8> = b"\x21".to_vec();
-  let dc2: Vec<u8> = b"\x18".to_vec();
-  let dc4: Vec<u8> = b"\x20".to_vec();
+  let nak: Vec<u8> = vec!(21);
+  let dc2: Vec<u8> = vec!(18);
+  let dc4: Vec<u8> = vec!(20);
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.get()")));
   let client = match redis::Client::open(format!("redis://{}/", HOST)) {
     Ok(client) => client,
@@ -59,9 +59,9 @@ pub extern "C" fn get(c: *const c_char) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn qget(c: *const c_char) -> *const c_char {
-  let nak: Vec<u8> = b"\x21".to_vec();
-  let dc2: Vec<u8> = b"\x18".to_vec();
-  let dc4: Vec<u8> = b"\x20".to_vec();
+  let nak: Vec<u8> = vec!(21);
+  let dc2: Vec<u8> = vec!(18);
+  let dc4: Vec<u8> = vec!(20);
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.qget()")));
   let client = match redis::Client::open(format!("redis://{}/", HOST)) {
     Ok(client) => client,
