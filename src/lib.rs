10eba01971b19c412b56c16c934c3817f0d876f3
Eduardo Tongson <propolice@gmail.com>
2020-12-12 20:55:02 +0800

    cargo fmt.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@ use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::panic;
 
-extern crate redis;
 extern crate base64;
+extern crate redis;
 extern crate serde_json;
-use std::collections::HashMap;
-use serde::{Deserialize};
+use serde::Deserialize;
 use serde_json::from_slice;
+use std::collections::HashMap;
 
 const HOST: &str = "127.0.0.1";
 
@@ -15,15 +15,15 @@ fn cs(s: Vec<u8>) -> *const c_char {
   let c_str = CString::new(s).unwrap();
   let ptr = c_str.as_ptr();
   std::mem::forget(c_str);
-  return ptr
+  return ptr;
 }
 
 #[no_mangle]
 pub extern "C" fn del(c: *const c_char) -> *const c_char {
-  let ack: Vec<u8> = vec!(6);
-  let dc2: Vec<u8> = vec!(18);
-  let dc4: Vec<u8> = vec!(20);
-  let nak: Vec<u8> = vec!(21);
+  let ack: Vec<u8> = vec![6];
+  let dc2: Vec<u8> = vec![18];
+  let dc4: Vec<u8> = vec![20];
+  let nak: Vec<u8> = vec![21];
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.del()")));
   let client = match redis::Client::open(format!("redis://{}/", HOST)) {
     Ok(rc) => rc,
@@ -34,7 +34,7 @@ pub extern "C" fn del(c: *const c_char) -> *const c_char {
     Err(_) => return cs(dc4),
   };
   let cb = unsafe { CStr::from_ptr(c).to_bytes() };
-  let _ : () = match redis::cmd("DEL").arg(cb).query::<i32>(&mut con) {
+  let _: () = match redis::cmd("DEL").arg(cb).query::<i32>(&mut con) {
     Ok(_) => return cs(ack),
     Err(_) => return cs(nak),
   };
@@ -42,10 +42,10 @@ pub extern "C" fn del(c: *const c_char) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn incr(c: *const c_char) -> *const c_char {
-  let ack: Vec<u8> = vec!(6);
-  let dc2: Vec<u8> = vec!(18);
-  let dc4: Vec<u8> = vec!(20);
-  let nak: Vec<u8> = vec!(21);
+  let ack: Vec<u8> = vec![6];
+  let dc2: Vec<u8> = vec![18];
+  let dc4: Vec<u8> = vec![20];
+  let nak: Vec<u8> = vec![21];
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.incr()")));
   let client = match redis::Client::open(format!("redis://{}/", HOST)) {
     Ok(rc) => rc,
@@ -56,7 +56,7 @@ pub extern "C" fn incr(c: *const c_char) -> *const c_char {
     Err(_) => return cs(dc4),
   };
   let cb = unsafe { CStr::from_ptr(c).to_bytes() };
-  let _ : () = match redis::cmd("INCR").arg(cb).query::<i32>(&mut con) {
+  let _: () = match redis::cmd("INCR").arg(cb).query::<i32>(&mut con) {
     Ok(_) => return cs(ack),
     Err(_) => return cs(nak),
   };
@@ -64,9 +64,9 @@ pub extern "C" fn incr(c: *const c_char) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn get(c: *const c_char) -> *const c_char {
-  let nak: Vec<u8> = vec!(21);
-  let dc2: Vec<u8> = vec!(18);
-  let dc4: Vec<u8> = vec!(20);
+  let nak: Vec<u8> = vec![21];
+  let dc2: Vec<u8> = vec![18];
+  let dc4: Vec<u8> = vec![20];
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.get()")));
   let client = match redis::Client::open(format!("redis://{}/", HOST)) {
     Ok(client) => client,
@@ -77,7 +77,7 @@ pub extern "C" fn get(c: *const c_char) -> *const c_char {
     Err(_) => return cs(dc4),
   };
   let cb = unsafe { CStr::from_ptr(c).to_bytes() };
-  let _ :() = match redis::cmd("GET").arg(cb).query::<Vec<u8>>(&mut con) {
+  let _: () = match redis::cmd("GET").arg(cb).query::<Vec<u8>>(&mut con) {
     Ok(s) => return cs(base64::encode(s).as_bytes().to_vec()),
     Err(_) => return cs(nak),
   };
@@ -85,9 +85,9 @@ pub extern "C" fn get(c: *const c_char) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn qget(c: *const c_char) -> *const c_char {
-  let nak: Vec<u8> = vec!(21);
-  let dc2: Vec<u8> = vec!(18);
-  let dc4: Vec<u8> = vec!(20);
+  let nak: Vec<u8> = vec![21];
+  let dc2: Vec<u8> = vec![18];
+  let dc4: Vec<u8> = vec![20];
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.qget()")));
   let client = match redis::Client::open(format!("redis://{}/", HOST)) {
     Ok(client) => client,
@@ -98,7 +98,7 @@ pub extern "C" fn qget(c: *const c_char) -> *const c_char {
     Err(_) => return cs(dc4),
   };
   let cb = unsafe { CStr::from_ptr(c).to_bytes() };
-  let _ :() = match redis::cmd("GET").arg(cb).query::<Vec<u8>>(&mut con) {
+  let _: () = match redis::cmd("GET").arg(cb).query::<Vec<u8>>(&mut con) {
     Ok(s) => return cs(s),
     Err(_) => return cs(nak),
   };
@@ -106,8 +106,8 @@ pub extern "C" fn qget(c: *const c_char) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn set(c: *const c_char) -> *const c_char {
-  let dc2: Vec<u8> = vec!(18);
-  let dc4: Vec<u8> = vec!(20);
+  let dc2: Vec<u8> = vec![18];
+  let dc4: Vec<u8> = vec![20];
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.set()")));
   #[derive(Deserialize)]
   struct Args {
@@ -125,30 +125,37 @@ pub extern "C" fn set(c: *const c_char) -> *const c_char {
   let cb = unsafe { CStr::from_ptr(c).to_bytes() };
   let j: Args = from_slice(cb).unwrap();
   let d: HashMap<String, String> = j.data;
-  let mut ret: Vec<u8> = vec!(6);
+  let mut ret: Vec<u8> = vec![6];
   match j.expire.as_str() {
     "0" => {
       for (k, v) in &d {
-        let _ : () = match redis::cmd("SET").arg(k).arg(v).query::<String>(&mut con) {
-          Ok(_) => {},
-          Err(_) => { ret = vec!(21); break; },
+        let _: () = match redis::cmd("SET").arg(k).arg(v).query::<String>(&mut con) {
+          Ok(_) => {}
+          Err(_) => {
+            ret = vec![21];
+            break;
+          }
         };
       }
       return cs(ret);
-    },
+    }
     _ => {
       for (k, v) in &d {
-        let _ : () = match redis::cmd("SET")
+        let _: () = match redis::cmd("SET")
           .arg(k)
           .arg(v)
           .arg("EX")
           .arg(&j.expire)
-          .query::<String>(&mut con) {
-            Ok(_) => {},
-            Err(_) => { ret = vec!(21); break; },
-          };
+          .query::<String>(&mut con)
+        {
+          Ok(_) => {}
+          Err(_) => {
+            ret = vec![21];
+            break;
+          }
+        };
       }
       return cs(ret);
-    },
+    }
   };
 }
