2a7ceac537d7b04078049a8234f9ba36aff3e3bd
Eduardo Tongson <propolice@gmail.com>
2020-12-13 19:07:36 +0800

    Unlink.

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,28 @@ pub extern "C" fn del(c: *const c_char) -> *const c_char {
 }
 
 #[no_mangle]
+pub extern "C" fn unlink(c: *const c_char) -> *const c_char {
+  let ack: Vec<u8> = vec![6];
+  let dc2: Vec<u8> = vec![18];
+  let dc4: Vec<u8> = vec![20];
+  let nak: Vec<u8> = vec![21];
+  panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.unlink()")));
+  let client = match redis::Client::open(format!("redis://{}/", HOST)) {
+    Ok(rc) => rc,
+    Err(_) => return cs(dc2),
+  };
+  let mut con = match client.get_connection() {
+    Ok(rc) => rc,
+    Err(_) => return cs(dc4),
+  };
+  let cb = unsafe { CStr::from_ptr(c).to_bytes() };
+  let _: () = match redis::cmd("UNLINK").arg(cb).query::<i32>(&mut con) {
+    Ok(_) => return cs(ack),
+    Err(_) => return cs(nak),
+  };
+}
+
+#[no_mangle]
 pub extern "C" fn incr(c: *const c_char) -> *const c_char {
   let ack: Vec<u8> = vec![6];
   let dc2: Vec<u8> = vec![18];
