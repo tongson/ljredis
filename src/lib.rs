7cfc7eecb56f11917d7de6495fa5d5cfd2df7a5e
Eduardo Tongson <propolice@gmail.com>
2020-12-30 15:27:28 +0800

    src: Should be a thin shim. Do all base64() outside.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::panic;
 
-extern crate base64;
 extern crate redis;
 extern crate serde_json;
 use serde::Deserialize;
@@ -87,27 +86,6 @@ pub extern "C" fn incr(c: *const c_char) -> *const c_char {
 #[no_mangle]
 pub extern "C" fn get(c: *const c_char) -> *const c_char {
   panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.get()")));
-  let nak: Vec<u8> = vec![21];
-  let dc2: Vec<u8> = vec![18];
-  let dc4: Vec<u8> = vec![20];
-  let client = match redis::Client::open(format!("redis://{}/", HOST)) {
-    Ok(client) => client,
-    Err(_) => return cs(dc2),
-  };
-  let mut con = match client.get_connection() {
-    Ok(con) => con,
-    Err(_) => return cs(dc4),
-  };
-  let cb = unsafe { CStr::from_ptr(c).to_bytes() };
-  let _: () = match redis::cmd("GET").arg(cb).query::<Vec<u8>>(&mut con) {
-    Ok(s) => return cs(base64::encode(s).as_bytes().to_vec()),
-    Err(_) => return cs(nak),
-  };
-}
-
-#[no_mangle]
-pub extern "C" fn qget(c: *const c_char) -> *const c_char {
-  panic::set_hook(Box::new(move |_| eprintln!("panic: rediz.qget()")));
   let nak: Vec<u8> = vec![21];
   let dc2: Vec<u8> = vec![18];
   let dc4: Vec<u8> = vec![20];
